f2315f283e8cd42dcd18841b455baf5992279c28
drdh <drdh@mail.ustc.edu.cn>
2019-03-27 00:57:26 +0800

    Easy Part Finished

--- Computer-Arch/ComputerArchitectureLab/1_VerilogSourceCode/1_CPUCore_src/IDSegReg.v
+++ Computer-Arch/ComputerArchitectureLab/1_VerilogSourceCode/1_CPUCore_src/IDSegReg.v
@@ -34,7 +34,7 @@ module IDSegReg(
     wire [31:0] RD_raw;
     InstructionRam InstructionRamInst (
          .clk    (clk),                        //请完善代码!!!
-         .addra  (A),                        //请完善代码!!!
+         .addra  (A[31:2]),                        //请完善代码!!!
          .douta  ( RD_raw     ),
          .web    ( |WE2       ),
          .addrb  ( A2[31:2]   ),

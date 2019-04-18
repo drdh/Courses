c3e31ea63ff8d3ce2919c0bb480f85349dabb343
drdh <drdh@mail.ustc.edu.cn>
2019-04-18 10:21:04 +0800

    lab3

--- Parallel-Computing/Lab3/matrix_matrix.cu
+++ Parallel-Computing/Lab3/matrix_matrix.cu
@@ -10,57 +10,57 @@ using namespace std;
 #define BLOCK_SIZE 4
 
 
-typedef void (*multiply)(int *A, int *B, int *C,int width);
+typedef void (*multiply)(int *A, int *B, int *C,int BLOCK_NUM);
 
-double getTime(int *A, int *B, int *C, multiply mul,int width){
+double getTime(int *A, int *B, int *C, multiply mul,int BLOCK_NUM){
     timeval start, finish;
     gettimeofday(&start, 0);
-    mul(A, B, C,width);
+    mul(A, B, C,BLOCK_NUM);
     gettimeofday(&finish, 0);
     double interval = 1e6 * (finish.tv_sec - start.tv_sec) + finish.tv_usec - start.tv_usec;
     return interval;
 }
 
-void searial(int *A, int *B, int *C,int width){
-    for (int i = 0; i < width * BLOCK_SIZE; i++){
-        for (int j = 0; j < width * BLOCK_SIZE; j++){
+void searial(int *A, int *B, int *C,int BLOCK_NUM){
+    for (int i = 0; i < BLOCK_NUM * BLOCK_SIZE; i++){
+        for (int j = 0; j < BLOCK_NUM * BLOCK_SIZE; j++){
             int sum = 0;
-            for (int k = 0; k < width * BLOCK_SIZE; k++){
-                sum += A[i * width * BLOCK_SIZE + k] * B[k * width * BLOCK_SIZE + j];
+            for (int k = 0; k < BLOCK_NUM * BLOCK_SIZE; k++){
+                sum += A[i * BLOCK_NUM * BLOCK_SIZE + k] * B[k * BLOCK_NUM * BLOCK_SIZE + j];
             }
-            C[i * width * BLOCK_SIZE + j] = sum;
+            C[i * BLOCK_NUM * BLOCK_SIZE + j] = sum;
         }
     }
 }
 
 __global__
-void deviceParallel1(int *A, int *B, int *C,int width){
+void deviceParallel1(int *A, int *B, int *C,int BLOCK_NUM){
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
     int sum = 0;
-    for (int i = 0; i < width * BLOCK_SIZE; i++){
-        sum += A[row * width * BLOCK_SIZE + i] * B[i * width * BLOCK_SIZE + col];
+    for (int i = 0; i < BLOCK_NUM * BLOCK_SIZE; i++){
+        sum += A[row * BLOCK_NUM * BLOCK_SIZE + i] * B[i * BLOCK_NUM * BLOCK_SIZE + col];
     }
-    C[row * width * BLOCK_SIZE + col] = sum;
+    C[row * BLOCK_NUM * BLOCK_SIZE + col] = sum;
 }
 
-void parallel1(int *A, int *B, int *C,int width){
+void parallel1(int *A, int *B, int *C,int BLOCK_NUM){
     int *CA, *CB, *CC;
-    cudaMalloc(&CA, sizeof(int) * width * BLOCK_SIZE * width * BLOCK_SIZE);
-    cudaMalloc(&CB, sizeof(int) * width * BLOCK_SIZE * width * BLOCK_SIZE); 
-    cudaMalloc(&CC, sizeof(int) * width * BLOCK_SIZE * width * BLOCK_SIZE);
-    cudaMemcpy(CA, A, sizeof(int) * width * BLOCK_SIZE * width * BLOCK_SIZE, 
+    cudaMalloc(&CA, sizeof(int) * BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE);
+    cudaMalloc(&CB, sizeof(int) * BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE); 
+    cudaMalloc(&CC, sizeof(int) * BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE);
+    cudaMemcpy(CA, A, sizeof(int) * BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE, 
                                         cudaMemcpyHostToDevice);
 
-    cudaMemcpy(CB, B, sizeof(int) * width * BLOCK_SIZE * width * BLOCK_SIZE, 
+    cudaMemcpy(CB, B, sizeof(int) * BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE, 
                                         cudaMemcpyHostToDevice);
 
     dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE); 
-    dim3 dimGrid(width, width);
-    deviceParallel1<<<dimBlock, dimGrid>>>(CA, CB, CC,width);
+    dim3 dimGrid(BLOCK_NUM, BLOCK_NUM);
+    deviceParallel1<<<dimBlock, dimGrid>>>(CA, CB, CC,BLOCK_NUM);
     //cudaThreadSynchronize();
     cudaDeviceSynchronize();
-    cudaMemcpy(C, CC, sizeof(int) * width * BLOCK_SIZE * width * BLOCK_SIZE, 
+    cudaMemcpy(C, CC, sizeof(int) * BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE, 
                                         cudaMemcpyDeviceToHost);
     cudaFree(CA);
     cudaFree(CB);
@@ -68,7 +68,7 @@ void parallel1(int *A, int *B, int *C,int width){
 }
 
 __global__
-void deviceParallel2(int *A, int *B, int *C,int width){
+void deviceParallel2(int *A, int *B, int *C,int BLOCK_NUM){
     //获得线程块号
     int blkRow = blockIdx.y; 
     int blkCol = blockIdx.x;
@@ -80,15 +80,15 @@ void deviceParallel2(int *A, int *B, int *C,int width){
     int var = 0;
 
     //循环，遍历所有子矩阵
-    for (int i = 0; i < width; i++) {   
-        const int *ASub = A + blkRow * BLOCK_SIZE * width + i * BLOCK_SIZE; 
-        const int *BSub = B + i * BLOCK_SIZE * width + blkCol * BLOCK_SIZE;
+    for (int i = 0; i < BLOCK_NUM; i++) {   
+        const int *ASub = A + blkRow * BLOCK_SIZE * BLOCK_NUM + i * BLOCK_SIZE; 
+        const int *BSub = B + i * BLOCK_SIZE * BLOCK_NUM + blkCol * BLOCK_SIZE;
 
         __shared__ int Ads[BLOCK_SIZE][BLOCK_SIZE]; 
         __shared__ int Bds[BLOCK_SIZE][BLOCK_SIZE];
 
-        Ads[row][col] = *(ASub + row * BLOCK_SIZE * width + col); 
-        Bds[row][col] = *(BSub + row * BLOCK_SIZE * width + col);
+        Ads[row][col] = *(ASub + row * BLOCK_SIZE * BLOCK_NUM + col); 
+        Bds[row][col] = *(BSub + row * BLOCK_SIZE * BLOCK_NUM + col);
 
         __syncthreads();
 
@@ -97,31 +97,31 @@ void deviceParallel2(int *A, int *B, int *C,int width){
         }
         __syncthreads();
     }
-    int *CSub = C + blkRow * BLOCK_SIZE * width + blkCol * BLOCK_SIZE;
-    *(CSub + row * BLOCK_SIZE * width + col) = var;
+    int *CSub = C + blkRow * BLOCK_SIZE * BLOCK_NUM + blkCol * BLOCK_SIZE;
+    *(CSub + row * BLOCK_SIZE * BLOCK_NUM + col) = var;
 }
 
-void parallel2(int *A, int *B, int *C,int width){
+void parallel2(int *A, int *B, int *C,int BLOCK_NUM){
     int *CA, *CB, *CC;
-    cudaMalloc(&CA, sizeof(int) * width * BLOCK_SIZE * width * BLOCK_SIZE);
-    cudaMalloc(&CB, sizeof(int) * width * BLOCK_SIZE * width * BLOCK_SIZE); 
-    cudaMalloc(&CC, sizeof(int) * width * BLOCK_SIZE * width * BLOCK_SIZE);
+    cudaMalloc(&CA, sizeof(int) * BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE);
+    cudaMalloc(&CB, sizeof(int) * BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE); 
+    cudaMalloc(&CC, sizeof(int) * BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE);
 
-    cudaMemcpy(CA, A, sizeof(int) * width * BLOCK_SIZE * width * BLOCK_SIZE, 
+    cudaMemcpy(CA, A, sizeof(int) * BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE, 
                                         cudaMemcpyHostToDevice);
 
-    cudaMemcpy(CB, B, sizeof(int) * width * BLOCK_SIZE * width * BLOCK_SIZE, 
+    cudaMemcpy(CB, B, sizeof(int) * BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE, 
                                         cudaMemcpyHostToDevice);
 
     dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE); 
-    dim3 dimGrid(width, width);
+    dim3 dimGrid(BLOCK_NUM, BLOCK_NUM);
 
-    deviceParallel2<<<dimBlock, dimGrid>>>(CA, CB, CC,width);
+    deviceParallel2<<<dimBlock, dimGrid>>>(CA, CB, CC,BLOCK_NUM);
 
     //cudaThreadSynchronize();
     cudaDeviceSynchronize();
 
-    cudaMemcpy(C, CC, sizeof(int) * width * BLOCK_SIZE * width * BLOCK_SIZE, 
+    cudaMemcpy(C, CC, sizeof(int) * BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE, 
                                         cudaMemcpyDeviceToHost);
 
     cudaFree(CA);
@@ -140,24 +140,24 @@ void read(int *M, int row, int col){
 
 
 int main(int argc, char const *argv[]){
-    int width=4;
+    int BLOCK_NUM=4;
 
-    int *A = new int[width * BLOCK_SIZE * width * BLOCK_SIZE];
-    int *B = new int[width * BLOCK_SIZE * width * BLOCK_SIZE];
-    int *C1 = new int[width * BLOCK_SIZE * width * BLOCK_SIZE];
-    int *C2 = new int[width * BLOCK_SIZE * width * BLOCK_SIZE];
-    int *C3 = new int[width * BLOCK_SIZE * width * BLOCK_SIZE];
+    int *A = new int[BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE];
+    int *B = new int[BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE];
+    int *C1 = new int[BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE];
+    int *C2 = new int[BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE];
+    int *C3 = new int[BLOCK_NUM * BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE];
 
     // 读取矩阵数据
-    read(A, width * BLOCK_SIZE, width * BLOCK_SIZE);
-    read(B, width * BLOCK_SIZE, width * BLOCK_SIZE);
+    read(A, BLOCK_NUM * BLOCK_SIZE, BLOCK_NUM * BLOCK_SIZE);
+    read(B, BLOCK_NUM * BLOCK_SIZE, BLOCK_NUM * BLOCK_SIZE);
 
-    cout << "Serial Time = " << getTime(A, B, C1, searial,width) << " ps." << endl;
-    cout << "Parallel1 Time = " << getTime(A, B, C2, parallel1,width) << " ps." << endl;
-    //cout << "Parallel2 Time = " << getTime(A, B, C3, parallel2,width) << " ps." << endl;
+    cout << "Serial Time = " << getTime(A, B, C1, searial,BLOCK_NUM) << " ps." << endl;
+    cout << "Parallel1 Time = " << getTime(A, B, C2, parallel1,BLOCK_NUM) << " ps." << endl;
+    //cout << "Parallel2 Time = " << getTime(A, B, C3, parallel2,BLOCK_NUM) << " ps." << endl;
 
     
-    for(int i=0;i<width* BLOCK_SIZE * width * BLOCK_SIZE;i++){
+    for(int i=0;i<BLOCK_NUM* BLOCK_SIZE * BLOCK_NUM * BLOCK_SIZE;i++){
         if(C1[i]!=C2[i]|| C1[i]!=C3[i] ){
             cout<<"error "<<C1[i]<<","<<C2[i]<<","<<C3[i]<<endl;
         }
